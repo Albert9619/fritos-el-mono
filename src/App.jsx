b4b07cb7ff1305e4ceec006422076148e0f2dfa3
Albert9619 <alberto.ortega1220@gmail.com>
2026-03-26 23:08:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 // ==========================================
 // 🔴 CONTROL DE INVENTARIO ( Carepa )
 // ==========================================
-const agotados = ["Empanada Crujiente]; 
+const agotados = ["Empanada Crujiente"]
 
 const productosBase = [
   { id: 1, nombre: "Empanada Crujiente", precio: 1500, tieneSabor: true, opciones: ["Carne", "Pollo", "Arroz"], imagen: "/empanada.jpg" },
