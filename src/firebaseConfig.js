376bdc6f1108756ae6b376c99e9438ccd7f337d3
Albert9619 <alberto.ortega1220@gmail.com>
2026-03-30 15:15:46 -0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Datos reales de Fritos El Mono extraídos de tu consola
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyAIbKCPKMOauqkSB9UPt0GeslVEXetN2VA",
   authDomain: "fritos-el-mono.firebaseapp.com",
   projectId: "fritos-el-mono",
-  storageBucket: "fritos-el-mono.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  storageBucket: "fritos-el-mono.firebasestorage.app",
+  messagingSenderId: "697130752446",
+  appId: "1:697130752446:web:fae140a69d06e40cf201ef",
+  measurementId: "G-5423R2PQ0Y"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); // 👈 Asegúrate de que diga EXPORT
+export const db = getFirestore(app);
